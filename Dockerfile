d36ae51d26f1ec7351e9c96807870fbf474c8338
Khalid Ahmed <khalida@Khalids-MacBook-Pro.local>
2017-08-13 11:37:02 -0400

    Added files

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+# Build: docker build -t conductor-spark:2.2 .
+# Run:   docker run -it --net=host --privileged -e MASTERNAME=cfc-master conductor-spark:2.2
+# Run:   docker run -it --net=host --cap-add SYS_NICE --cap-add NET_BIND_SERVICE conductor-spark:2.2 
+FROM centos:7
+MAINTAINER Khalid Ahmed <khalida@ca.ibm.com>
+
+ENV CLUSTERADMIN egoadmin
+ENV CLUSTERNAME cws
+ENV BASEPORT 7869
+ENV DISABLESSL Y
+ENV LANG en_CA.UTF-8
+ENV CWS_INSTALL_PKG cwseval-2.2.0.0_x86_64.bin
+ENV CWS_CLUSTER_TOP  /opt/ibm/spectrumcomputing
+
+RUN useradd egoadmin
+RUN yum install -y gettext net-tools gawk which sudo tar wget
+
+RUN cd /;wget --quiet --no-proxy http://cfc-master:9191/${CWS_INSTALL_PKG};chmod 755 /${CWS_INSTALL_PKG};/${CWS_INSTALL_PKG} --quiet; rm -f /${CWS_INSTALL_PKG}
+
+COPY bootstrap.sh /bootstrap.sh
+RUN chmod 755 /bootstrap.sh
+
+CMD /bootstrap.sh
+
+
---
+++ bootstrap.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+export CWS_TOP=/opt/ibm/spectrumcomputing
+export CLUSTERADMIN=egoadmin
+export HOSTNAME=`hostname`
+
+# patch to support kernel 4
+sed -i 's|$version = "3"|$version = "3" -o $version = "4"|g' ${SYM_TOP}/kernel/conf/profile.ego
+
+/bin/su -c "source $CWS_TOP/kernel/conf/profile.ego; egoconfig join $MASTERNAME -f" egoadmin
+if [ "$MASTERNAME" == `hostname` ]; then
+    cat /var/tmp/cfc/key1.dat >> /tmp/license.dat
+    sed -i -e '$a\' /tmp/license.dat
+    cat /var/tmp/cfc/key2.dat >> /tmp/license.dat
+    /bin/su -c "source $CWS_TOP/profile.platform; egoconfig mghost /shared -f; source $CWS_TOP/profile.platform; egoconfig setentitlement /tmp/license.dat" egoadmin
+fi
+
+source $CWS_TOP/profile.platform
+
+chown  $CLUSTERADMIN $CWS_TOP/kernel/work 
+chgrp  $CLUSTERADMIN $CWS_TOP/kernel/work 
+egosh ego start
+
+i=1
+while [ "$i" -ne 0 ]
+do
+   sleep 10
+done
+
